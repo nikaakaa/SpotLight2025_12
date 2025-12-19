45bb57f30c36703bb8f7d73569227142901dbe30
nikaakaa <3212380934@qq.com>
2025-12-19 19:18:21 +0800

    修改了些流程控制

--- Assets/Scripts/Core/GameProcedure/GameProcedureStates/MainMenuState.cs
+++ Assets/Scripts/Core/GameProcedure/GameProcedureStates/MainMenuState.cs
@@ -1,10 +1,17 @@
+using System.IO;
+using SimpleJSON;
 using UnityEngine;
+using cfg;
+using UnityEngine.SceneManagement; // 添加命名空间引用
 
 /// <summary>
 /// 主菜单状态 - 处理主菜单界面的显示、交互和导航逻辑
 /// </summary>
 public class MainMenuState : LeafState<GameProcedureContext>
 {
+    private static bool s_tablesRegistered;
+    private static Tables s_tables;
+
     public MainMenuState()
     {
         Name = nameof(MainMenuState);
@@ -13,6 +20,13 @@ public class MainMenuState : LeafState<GameProcedureContext>
     protected override void OnEnter(GameProcedureContext ctx)
     {
         Debug.Log($"[{Name}] Enter - 显示主菜单界面");
+        
+        // 如果当前不在主菜单场景，则加载
+        if (SceneManager.GetActiveScene().name != "MainScene")
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+        
         // TODO: 显示主菜单 UI
         // TODO: 初始化菜单按钮事件
     }
